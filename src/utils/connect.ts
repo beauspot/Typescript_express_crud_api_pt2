81b0f139de11015dcb158c99d5723028800c95c5
beauspot <beauspot@outlook.com>
2023-07-03 14:44:23 +0100

    implemented the logger on database conection to setup the database.

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import config from "config";
+import { logger } from "./logger";
 
 export const connect = async () => {
   const dbUri = config.get<string>("dbUri");
   try {
     const dbConnected = await mongoose.connect(dbUri);
-    console.log("Connected to the database");
+    logger.info("Connected to the database");
     return dbConnected;
   } catch (error) {
-      console.error(error);
-      process.exit(1);
+    logger.error(error);
+    process.exit(1);
   }
 };
