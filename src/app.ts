fc8be3a122deb81ced76eddae577bafdafd6855f
beauspot <beauspot@outlook.com>
2023-07-03 13:56:57 +0100

    slightly modified the entery point of the application.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello world from typescript express");
+import express from "express";
+import config from "config";
+import { connect } from "../src/utils/connect";
+
+const port = config.get<number>("port");
+const app = express();
+
+const startServer = async () => {
+  try {
+    await connect();
+    app.listen(port, () => console.log("Server Started successfully."));
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+startServer();
